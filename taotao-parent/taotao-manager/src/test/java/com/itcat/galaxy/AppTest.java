14aa1fdfd09e4807ac42cbef198b9d270d666041
ZhangJian <guigu9518@gmail.com>
2019-03-07 22:55:01 +0800

    delete .idea

---
+++ taotao-parent/taotao-manager/src/test/java/com/itcat/galaxy/AppTest.java
@@ -0,0 +1,18 @@
+package com.itcat.galaxy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
